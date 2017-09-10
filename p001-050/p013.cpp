80ac93f97a62137a48f80940ce74be3083a42f60
s1200145 <winning.eleven556@gmail.com>
2017-09-10 19:58:17 +0900

    update to correct answer

--- p001-050/p013.cpp
+++ p001-050/p013.cpp
@@ -1,58 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 20
+#define N 50
 #define ELEM 4
-void printDeque(const deque<int>& deq);
+void printVector(const vector<int>& vec);
 
 int main(){
-   string filename = "p013.txt";
-   vector<int> v(50, 0);
+   string fileName = "p013.txt";
+   vector<int> v(N, 0), val(N, 0);
    int i, j;
    ulong multi;
-   ulong max = 0;
    ifstream readFile;
    string readLineBuffer;
-   deque<ulong> q;
 
-   readFile.open(filename, ios::in);
+   readFile.open(fileName, ios::in);
 
    while(getline(readFile, readLineBuffer)){
-      // int tmp = atoi(readLineBuffer.c_str());
-      
-      int tmp[50];
       for(i = readLineBuffer.size()-1; i >= 0; i--){
-         tmp[readLineBuffer.size()-1-i] = readLineBuffer[i] - '0';
+         val[readLineBuffer.size()-1-i] = readLineBuffer[i] - '0';
       }
-      for(i = 0; i < 50; i++){
-         cout << tmp[i];
-      }
-      cout << endl;
+      // printVector(val);
       
-      for(i = 0; i < 50; i++){
-         
+      for(i = 0; i < N; i++){
+         v[i] += val[i];
       }
-      // arr[i][j] = tmp;
-      // if(j < N-1){
-      //    j++;
-      // }else{
-      //    j = 0;
-      //    i++;
-      // }
+      // printVector(v);
+      i = 0;
+      while(i < v.size()){
+         if(v[i] >= 10){
+            if(i == v.size()-1){
+               v.push_back(0);
+            }
+            v[i+1]++;
+            v[i] %= 10;
+         }else{
+            v[i] = v[i];
+         }
+         i++;
+      }
+      // printVector(v);
    }
 
-   // for(i = 0; i < N; i++){
-   //    for(j = 0; j < N; j++){
-   //       cout << arr[i][j] << ' ';
-   //    }
-   //    cout << endl;
-   // }
+   for(i = v.size()-1; i >= 0; i--){
+      cout << v[i];
+   }
+   cout << endl;
+   cout << v.size() << endl;
 }
 
-void printDeque(const deque<int>& deq){
-   const deque<int>::const_iterator itEnd = deq.end();
-   for(deque<int>::const_iterator it = deq.begin(); it != itEnd; ++it){
-      cout << *it << ' ';
+void printVector(const vector<int>& vec){
+   const vector<int>::const_iterator itEnd = vec.end();
+   for(vector<int>::const_iterator it = vec.begin(); it != itEnd; ++it){
+      cout << *it;
    }
    cout << endl;
 }
